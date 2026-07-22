51fdcba9b698c4c28608c03b5a07f0a977df3954
Aunshika Gorantla <gaunshika@gmail.com>
2026-07-22 13:55:46 +0530

    Fix production frontend configuration

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,15 +1,23 @@
-const GOOGLE_CLIENT_ID = "818713072122-g3aln31415tecrea2hv2nj72htd1ckna.apps.googleusercontent.com";
-const RECAPTCHA_SITE_KEY = "PASTE_YOUR_RECAPTCHA_SITE_KEY_HERE";
+const GOOGLE_CLIENT_ID =
+  "818713072122-g3aln31415tecrea2hv2nj72htd1ckna.apps.googleusercontent.com";
+
+const RECAPTCHA_SITE_KEY =
+  "6Le6UVgtAAAAAFsyBML3xCvwmV_KidonmaQqfsA5";
 
 window.__recaptchaLoaded = false;
+
 window.onRecaptchaLoaded = function () {
   window.__recaptchaLoaded = true;
   window.dispatchEvent(new Event("recaptcha-loaded"));
 };
 
 window.APP_CONFIG = {
-  API_BASE_URL: "http://localhost:8080",
-  GOOGLE_AUTH_API_BASE_URL: "http://localhost:8080",
+  API_BASE_URL:
+    "https://smart-inventory-sales-monitoring-system.onrender.com",
+
+  GOOGLE_AUTH_API_BASE_URL:
+    "https://smart-inventory-sales-monitoring-system.onrender.com",
+
   GOOGLE_CLIENT_ID,
   RECAPTCHA_SITE_KEY,
   RECAPTCHA_ENABLED: true
